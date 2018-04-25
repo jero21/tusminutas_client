e5d625cc7339277033f17cecd5507bd6e5efe280
Cristobal Berrios <elcriss96@gmail.com>
2018-04-25 13:42:35 -0300

    componente root de la app

---
+++ src/components/App/MinutaApp.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      enable-resize-watcher
+      fixed
+      app>
+      <v-toolbar
+        color="secondary"
+        dark>
+        <v-toolbar-title>Tu Minuta</v-toolbar-title>
+      </v-toolbar>
+      <v-list>
+        <v-subheader>Menu</v-subheader>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>assignment</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Mis Minutas</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Crear Minuta</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Tabla de Composición</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>local_pizza</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Mis Alimentos</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-divider></v-divider>
+        <v-subheader>Soporte</v-subheader>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>insert_comment</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Danos tu opinión</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click="alert('mostar las minutas')">
+          <v-list-tile-action>
+            <v-icon>error</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Notificar Error</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+      color="primary"
+      dark>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-jumbotron color="grey lighten-2">
+        <v-container fill-height>
+          <router-view/>
+        </v-container>
+      </v-jumbotron>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      miniVariant: false,
+      title: 'Mis Minutas'
+    }
+  }
+}
+</script>
+
