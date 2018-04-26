b607fb1298020c5536158b971ff6770c993590cf
Cristobal Berrios <elcriss96@gmail.com>
2018-04-26 00:11:32 -0300

    se agrega ruta para listar minutas y tabla de composicion

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MinutaApp from '@/components/App/MinutaApp'
+import MisMinutas from '@/components/Minuta/MisMinutas'
+import TablaComposicion from '@/components/TablaComposicion/TablaComposicion'
 
 Vue.use(Router)
 
@@ -8,7 +10,20 @@ export default new Router({
   routes: [
     {
       path: '/',
-      component: MinutaApp
+      component: MinutaApp,
+      redirect: 'minutas',
+      children: [
+        {
+          path: 'minutas',
+          name: 'Mis Minutas',
+          component: MisMinutas
+        },
+        {
+          path: 'tabla-composicion',
+          name: 'Tabla de Composición',
+          component: TablaComposicion
+        }
+      ]
     }
   ]
 })
