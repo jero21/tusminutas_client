15c38aad0e6500a06a84b0b72ca616803b48b0ba
Cristobal Berrios <elcriss96@gmail.com>
2018-04-26 03:09:23 -0300

    se agrega ruta de la api en desarrollo

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  address: {
+    api: "'http://localhost:8000/api/'"
+  }
 })
