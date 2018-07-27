8b1871976344220a25221cb5bafdf52bb117e748
Cristobal Berrios <elcriss96@gmail.com>
2018-07-26 20:06:53 -0400

    archivo configuracion de rutas

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MinutaApp from '@/components/App/MinutaApp'
 import MisMinutas from '@/components/Minuta/MisMinutas'
+import CrearMinuta from '@/components/Minuta/CrearMinuta'
 import TablaComposicion from '@/components/TablaComposicion/TablaComposicion'
+import MisAlimentos from '@/components/Alimentos/MisAlimentos'
+import Login from '@/components/App/Login'
+
+import Credentials from '@/services/Credentials.service.js'
+
+const credentials = new Credentials()
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -19,11 +26,37 @@ export default new Router({
           component: MisMinutas
         },
         {
+          path: 'crear-minuta',
+          name: 'Crear Minuta',
+          component: CrearMinuta
+        },
+        {
           path: 'tabla-composicion',
           name: 'Tabla de Composición',
           component: TablaComposicion
+        },
+        {
+          path: 'mis-alimentos',
+          name: 'Mis Alimentos',
+          component: MisAlimentos
         }
       ]
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (credentials.isLogin()) {
+    if (to.path === '/login') next({path: '/minutas'})
+    else next()
+  } else {
+    if (to.path === '/login') next()
+    else next({path: '/login'})
+  }
+})
+
+export default router
