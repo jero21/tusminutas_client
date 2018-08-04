34110687b8404a0d81e08c0193c39969554b6013
Cristobal Berrios <elcriss96@gmail.com>
2018-08-04 17:54:04 -0400

    se agrega btn guardar y btn configuracion

--- src/components/Minuta/CrearMinuta.vue
+++ src/components/Minuta/CrearMinuta.vue
@@ -1,6 +1,6 @@
 <template>
   <v-layout row wrap>
-      <v-flex xs12 >
+    <v-flex xs12 md8 >
       <v-expansion-panel>
         <v-expansion-panel-content>
           <div slot="header">TOTALES</div>
@@ -12,54 +12,77 @@
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-flex>
+    <v-flex xs12 md4>
+      <v-btn @click="dialogConfiguracion = true">Configuración Minuta
+        <v-icon right>settings</v-icon>
+      </v-btn>
+    </v-flex>
     <v-flex xs12>
       <v-stepper value="1">
         <v-stepper-header>
-          <template v-for="(comida, indexComida) in comidas">
+          <template v-for="(comida, indexComida) in minuta.comidas">
             <v-stepper-step editable :key="`${comida.id}-step`" :step="indexComida + 1">{{ comida.nombre }}</v-stepper-step>
-            <v-divider v-if="indexComida < comidas.length - 1" :key="comida.id"></v-divider>
+            <v-divider v-if="indexComida < minuta.comidas.length - 1" :key="comida.id"></v-divider>
           </template>
         </v-stepper-header>
         <v-stepper-items>
-          <v-stepper-content v-for="(comida, indexComida) in comidas" :key="comida.id" :step="indexComida + 1">
+          <v-stepper-content v-for="(comida, indexComida) in minuta.comidas" :key="comida.id" :step="indexComida + 1">
             <crear-comida-minuta :alimentosComida="comida.alimentosMinuta" :totales.sync="comida.totales"></crear-comida-minuta>
           </v-stepper-content>
         </v-stepper-items>
       </v-stepper>
     </v-flex>
     <v-flex offset-xs10 xs2>
-      <v-btn color="primary">Guardar</v-btn>
+      <v-btn @click="dialogGuardar = true" color="primary">Crear</v-btn>
     </v-flex>
+
+    <dialog-guardar-minta :minuta="minuta" @closeDialog="dialogGuardar = false" :dialog="dialogGuardar"></dialog-guardar-minta>
+    <configuracion-minuta :configuracionMinuta="configuracionMinuta" :dialog="dialogConfiguracion" @closeDialog="dialogConfiguracion = false" ></configuracion-minuta>
+
+
   </v-layout>
 </template>
 
 <script>
 import CrearComidaMinuta from '@/components/Minuta/CrearComidaMinuta'
+import DialogGuardarMinta from '@/components/Minuta/DialogGuardarMinuta'
+import ConfiguracionMinuta from '@/components/Minuta/ConfiguracionMinuta'
 
 let totales = {cantidad: 0, humedad: 0, energia: 0, proteinas: 0, carbohidratos: 0}
+let comidas = [
+  {id: 1, nombre: 'Desayuno', alimentosMinuta: [], totales: totales},
+  {id: 2, nombre: 'Almuerzo', alimentosMinuta: [], totales: totales},
+  {id: 3, nombre: 'Once', alimentosMinuta: [], totales: totales},
+  {id: 4, nombre: 'Cena', alimentosMinuta: [], totales: totales},
+  {id: 5, nombre: 'Colación 1', alimentosMinuta: [], totales: totales},
+  {id: 6, nombre: 'Colación 2', alimentosMinuta: [], totales: totales}
+]
 
 export default {
   data () {
     return {
-      comidas: [
-        {id: 1, nombre: 'Desayuno', alimentosMinuta: [], totales: totales},
-        {id: 2, nombre: 'Almuerzo', alimentosMinuta: [], totales: totales},
-        {id: 3, nombre: 'Once', alimentosMinuta: [], totales: totales},
-        {id: 4, nombre: 'Cena', alimentosMinuta: [], totales: totales},
-        {id: 5, nombre: 'Colación 1', alimentosMinuta: [], totales: totales},
-        {id: 6, nombre: 'Colación 2', alimentosMinuta: [], totales: totales}
-      ]
+      minuta: {
+        nombre: '',
+        descripcion: '',
+        id_tipo_minuta: 1,
+        comidas: comidas
+      },
+      configuracionMinuta: [],
+      dialogGuardar: false,
+      dialogConfiguracion: false
     }
   },
   components: {
-    CrearComidaMinuta
+    CrearComidaMinuta,
+    DialogGuardarMinta,
+    ConfiguracionMinuta
   },
   methods: {
   },
   computed: {
     totalCantidad () {
       let total = 0
-      this.comidas.forEach(comida => {
+      this.minuta.comidas.forEach(comida => {
         comida.alimentosMinuta.forEach(alimento => {
           total += alimento.cantidad
         })
