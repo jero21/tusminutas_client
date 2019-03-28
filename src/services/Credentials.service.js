b71e4338e9c811c515821342315830e1d8a0fe44
Cristobal Berrios <elcriss96@gmail.com>
2019-03-28 02:03:07 -0300

    se modifica el objecto user

--- src/services/Credentials.service.js
+++ src/services/Credentials.service.js
@@ -1,27 +1,17 @@
 class CredentialsService {
   setCurrentUser (user) {
-    localStorage.setItem('user', user)
+    localStorage.setItem('user', JSON.stringify(user))
   }
   getCurrentUser () {
     let user = localStorage.getItem('user')
-    return user
-  }
-
-  setToken (token) {
-    localStorage.setItem('token', token)
-  }
-  getToken () {
-    let token = localStorage.getItem('token')
-    return token
+    return JSON.parse(user)
   }
   clearCredentials () {
     localStorage.clear('user')
-    localStorage.clear('token')
   }
 
   isLogin () {
-    if (this.getToken() != null) return true
-    else return false
+    return localStorage.getItem('user')
   }
 }
 
