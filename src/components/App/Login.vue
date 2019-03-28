4a045c2a0b9849273eec1343c8776bdc629bb363
Cristobal Berrios <elcriss96@gmail.com>
2019-03-28 02:00:14 -0300

    se configura login google

--- src/components/App/Login.vue
+++ src/components/App/Login.vue
@@ -9,26 +9,26 @@
             <v-card class="elevation-12">
               <v-toolbar dark color="primary">
                 <v-toolbar-title>Tu Minuta</v-toolbar-title>
-                <v-spacer></v-spacer>
-                  <g-signin-button
-                    :params="googleSignInParams"
-                    @success="onSignInSuccess"
-                    @error="onSignInError">
-                      <v-btn color="blue">
-                        Sign in with Google
-                      </v-btn>
-                  </g-signin-button>
               </v-toolbar>
-              <v-card-text>
-                <v-form v-model="valid" ref="form" lazy-validation>
-                  <v-text-field :rules="emailRules" prepend-icon="person" name="login" label="Login" v-model="credentials.email" type="text" required></v-text-field>
-                  <v-text-field :rules="passwordRules" prepend-icon="lock" name="password" label="Password" v-model="credentials.password" type="password" required></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn :disabled="formValidado" @click="submit(credentials)" :loading="isLoading" color="primary">Iniciar Sesión</v-btn>
-              </v-card-actions>
+                <v-card-text>
+                  <v-layout row wrap>
+                    <v-flex xs12>
+                    <g-signin-button
+                        :params="googleSignInParams"
+                        @success="onSignInSuccess"
+                        @error="onSignInError">
+                          <v-btn :loading="loadingGoogle" block dark color="red">
+                            Iniciar Sesión con Google
+                          </v-btn>
+                    </g-signin-button>
+                    </v-flex>
+                    <v-flex xs12>
+                    <v-btn block dark color="#3b5998">
+                      Iniciar Sesión con Facebook
+                    </v-btn>
+                    </v-flex>
+                  </v-layout>
+                </v-card-text>
             </v-card>
           </v-flex>
         </v-layout>
@@ -46,7 +46,6 @@ export default {
   data () {
     return {
       valid: false,
-      isLoading: false,
       credentialService: new CredentialsServices(),
       emailRules: [
         v => !!v || 'Debe ingresar un e-mail',
@@ -56,33 +55,31 @@ export default {
         v => !!v || 'Debe ingresar una constraseña'
       ],
       credentials: {
-        email: '',
-        password: ''
+        access_token: ''
       },
       gradient: 'to top right,rgba(0,151,167,1),rgba(2,119,129,1) ',
       googleSignInParams: {
         client_id: '219352268007-7l6312ken2a5q09v1guf9kak3p39vfj1.apps.googleusercontent.com'
-      }
+      },
+      loadingGoogle: false
     }
   },
   methods: {
     submit (credentials) {
       let vm = this
-      vm.isLoading = true
+      vm.loadingGoogle = true
       loginService.authenticate(credentials).then(data => {
-        vm.credentialService.setToken(data.body.token)
-        vm.credentialService.setCurrentUser(data.body.email)
+        vm.credentialService.setCurrentUser(data.body)
         this.$router.push('/minutas')
-        console.log(data)
       }, err => {
-        vm.isLoading = false
+        vm.loagingGoogle = false
         console.log(err)
       })
     },
     onSignInSuccess (googleUser) {
-      const profile = googleUser.getBasicProfile()
-      console.log(profile)
-      console.log(googleUser)
+      let vm = this
+      vm.credentials.access_token = googleUser.Zi.access_token
+      vm.submit(vm.credentials)
     },
     onSignInError (error) {
       console.log('OH NOES', error)
