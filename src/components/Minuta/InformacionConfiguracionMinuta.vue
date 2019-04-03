7d8785cd1a28db20a89e4186065e169b2f9e86cc
Cristobal Berrios <elcriss96@gmail.com>
2019-04-03 01:10:14 -0300

    se elimnan datos que ya no se usan

--- src/components/Minuta/InformacionConfiguracionMinuta.vue
+++ src/components/Minuta/InformacionConfiguracionMinuta.vue
@@ -13,57 +13,6 @@
 
 <script>
 export default {
-  data () {
-    return {
-      propiedades: {
-        a_g_omega3: {},
-        a_g_omega6: {},
-        a_g_trans: {},
-        a_grasos_monosat: {},
-        a_grasos_polisat: {},
-        a_grasos_sat: {},
-        acido_folico: {},
-        acido_pantotenico: {},
-        biotina: {},
-        calcio: {},
-        carbohidratos: {},
-        cobre: {},
-        colesterol: {},
-        energia: {},
-        fibra: {},
-        fosforo: {},
-        grasas_totales: {},
-        hierro: {},
-        humedad: {},
-        magnesio: {},
-        manganeso: {},
-        potasio: {},
-        proteinas: {},
-        riboflavina: {},
-        selenio: {},
-        sodio: {},
-        tiamina: {},
-        yodo: {},
-        zinc: {},
-        vit_a: {},
-        vit_b6: {},
-        vit_b12: {},
-        vit_c: {},
-        vit_d: {},
-        vit_e: {},
-        vit_k: {}
-      }
-    }
-  },
-  computed: {
-    configuracion () {
-      let vm = this
-      vm.configuracionMinuta.forEach(propiedad => {
-        vm.propiedades[propiedad.nombre] = propiedad
-      })
-      return vm.propiedades
-    }
-  },
   props: ['configuracionMinuta']
 }
 </script>
